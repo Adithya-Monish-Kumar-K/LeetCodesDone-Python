9f7a228753a68b5e05ca1cf5c36840d24ae2f437
Adithya Monish Kumar K <141850177+Adithya-Monish-Kumar-K@users.noreply.github.com>
2025-07-12 02:07:33 +0530

    Delete H-index.py

--- H-index.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        h=len(citations)
-        citations.sort(reverse=True)
-        for i in range(h):
-            if citations[i] <i+1:
-                return i
-        return h
