5576024712138c4e51de9b5fc17ca3ded142ac27
Adithya Monish Kumar K <141850177+Adithya-Monish-Kumar-K@users.noreply.github.com>
2025-08-17 11:57:40 +0530

    25.py uploaded

---
+++ LeetCodesDone/25.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        d = ListNode(0, head)
+        gp = d
+        while True:
+            kth = gp
+            for _ in range(k):
+                kth = kth.next
+                if not kth:
+                    return d.next
+            gn = kth.next
+            prev, curr = gn, gp.next
+            while curr != gn:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+            tail = gp.next
+            gp.next = kth
+            gp = tail
